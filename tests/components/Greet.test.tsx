e7e33c8b16935de257dd1e5279fc63fd0b4c85e8
Patricio Miguel Osorio Osorio <patricio.osorio@agilityps.mx>
2026-02-23 12:17:58 -0600

    Testing React Components: 3- Testing Rendering

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,32 @@
+import { it, expect, describe } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Greet from '../../src/components/Greet';
+import '@testing-library/jest-dom/vitest';
+
+describe('Greet', () => {
+  it('should render "Hello" with the name when name is provided', () => {
+    // Arrange
+    render(<Greet name="John" />);
+
+    // Act
+    const heading = screen.getByRole('heading');
+
+    // Assert
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/john/i);
+
+    screen.debug();
+  });
+
+  it('should render login when name is not provided', () => {
+    // Arrange
+    render(<Greet />);
+
+    // Act
+    const button = screen.getByRole('button');
+
+    // Assert
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
+});
---
+++ tests/main.test.ts
@@ -0,0 +1,7 @@
+import { it, expect, describe } from 'vitest';
+
+describe('group', () => {
+  it('should something ...', () => {
+    expect(1).toBeTruthy();
+  });
+});
